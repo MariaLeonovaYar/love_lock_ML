7015d2aed61e1ee509b78e213ac052beb48f94dd
LebedevaNat <natali25lebedeva@gmail.com>
2019-12-11 13:24:02 +0300

    Главная страница на React

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "index.js";
+// import "App.css"
+
+class App extends React.Component {
+    render(){
+    return (
+      <div id="root" className="rect"></div>
+    );
+  }
+}
+
+export default App;

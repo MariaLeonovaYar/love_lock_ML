85590ff2bc97e7c56681a01dd53443ececffad9d
Maria Leonova <leonova-99@list.ru>
2020-01-06 16:36:23 +0300

    Связана функция удаления и кнопка

--- client/src/api/DeleteLock.js
+++ client/src/api/DeleteLock.js
@@ -7,11 +7,11 @@ export const delete_lock_id= (id_lock) => {
     return axios
         .post('http://localhost:5000/api/delete_lock_id', payload)
         .then(res => {
-            console.log('Данные успешно были отправлены');
-            return res.data;
+            console.log('Данные успешно были удалены', id_lock);
+            return true;
         })
         .catch((error) => {
-            console.log('Данные не были успешно отправлены');
+            console.log('Данные не были успешно удалены');
             console.log(error);
         });
 };
